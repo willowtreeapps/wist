2f09bf527e5562a80ee329cffc1f80fb85650ff4
Matt VanNuys <matt.vannuys@willowtreeapps.com>
2017-09-18 15:07:41 -0400

    Add some unit tests

---
+++ test/lib/cli.js
@@ -0,0 +1,32 @@
+const path = require('path'),
+    Rules = require('../../lib/rules'),
+    test = require('ava');
+
+const VALID_DIR = path.resolve(`${__dirname}/../fixtures/rules`),
+    INVALID_DIR = path.resolve(`${__dirname}/../fixtures/rules-invaliddir`);
+
+let rules;
+
+test.beforeEach(t => {
+    rules = new Rules();
+});
+
+test('error is thrown when given an invalid rules directory', t => {
+    t.throws(() => {
+        rules.load(INVALID_DIR);
+    });
+});
+
+test('error is not thrown when given a valid rules directory', t => {
+    t.notThrows(() => {
+        rules.load(VALID_DIR);
+    });
+});
+
+test('rules.get for a valid rule returns an object', t => {
+    rules.load(VALID_DIR);
+    
+    const rule = rules.get('fixture-rule');
+
+    t.is(typeof rule, 'object');
+});
