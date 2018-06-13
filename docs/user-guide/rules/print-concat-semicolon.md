4e649307b07a00c73302544607d14eaee2e4219e
Nish Tahir <nish.tahir@willowtreeapps.com>
2018-06-13 11:23:18 -0400

    Add rule for print concat

---
+++ docs/user-guide/rules/print-concat-semicolon.md
@@ -0,0 +1,14 @@
+---
+title: print-concat-semicolon
+layout: doc
+edit_link: https://github.com/willowtreeapps/wist/edit/master/docs/user-guide/rules/print-concat-semicolon
+sidebar: "user-guide"
+grouping: "rules"
+---
+
+# print-concat-semicolon
+
+Print concatenation must be done with a semicolon.
+
+## Version
+This rule was introduced in Wist 2.0.0.
---
+++ src/js/rules/print-concat-semicolon.js
@@ -0,0 +1,42 @@
+/**
+ * When constructing console strings from non-string objects, avoid '+' operator 
+ * which requires objects to be converted to strings first.
+ * This approach tends to be susceptible to crashes.
+ * 
+ * @description Print concatenation must be done with a semicolon.
+ * @since 2.0.0
+ */
+'use strict';
+
+module.exports = {
+    create(context) {
+        return {
+            'printStatement:enter': function (ast) {
+                ast.children.forEach(element => {
+                    let node = element.node;
+                    if (node.context == "expression") {
+                        let concatSymbol = element.children.findFirst(child => child.node.text == "+")
+                        if (concatSymbol != null) {
+                            context.report({
+                                message: `'+' should not be used in print statements. Consider using a ';' instead.`,
+                                loc: {
+                                    start: ast.node.start,
+                                    end: ast.node.stop
+                                }
+                            });
+                        }
+                        console.log(element);
+                    }
+                });
+                // let node = astNode.node;
+                // context.report({
+                //     message: `'print' statements should only be used for debugging purposes.`,
+                //     loc: {
+                //         start: node.start,
+                //         end: node.stop
+                //     }
+                // });
+            }
+        };
+    }
+};
